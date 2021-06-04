849d18efab6bfab91b6a34128a124db0c0a9ecd3
Oliver Forral <Oliver.Forral@gmail.com>
2021-06-04 09:28:40 -0700

    created FileProcessor

---
+++ 02-managing-files-and-directories/01-new-console-app/DataProcessor/FileProcessor.cs
@@ -0,0 +1,14 @@
+using static System.Console;
+namespace DataProcessor
+{
+    class FileProcessor
+    {
+        public string InputFilePath { get; }
+        public FileProcessor(string filePath) => InputFilePath = filePath;
+
+        public void Process()
+        {
+            WriteLine($"Begin process of {InputFilePath}");
+        }
+    }
+}
